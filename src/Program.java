275a6f0661ee0ce9b39ead290427df0a5841c2f2
kkatrina93 <kkatrina93@mail.ru>
2022-05-24 20:56:22 +0300

    Added my metod

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,9 @@
-public class Program
-{
-	public static void main(String[] args)
-	{
+public class Program {
+    public static void main(String[] args) {
+        kalinina();
+    }
 
-	}
+    public static void kalinina() {
+        System.out.println("Калинина Екатерина Анатольевна");
+    }
 }
