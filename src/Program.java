2e17a65dd78e8173a1bf0ef7c82d051bfe97057c
NikolaiHoretski <nikolai.horetski@gmail.com>
2022-05-24 20:57:10 +0300

    Added a new method

--- src/Program.java
+++ src/Program.java
@@ -1,16 +1,21 @@
-public class Program
-{
-	public static void main(String[] args) {
-		kislushchanka();
-		dubina();
-	}
 
-	public static void kislushchanka() {
-		System.out.println("Кислущенко Андрей Валерьевич");
-	}
+public class Program {
+    public static void main(String[] args) {
+        kislushchanka();
+        horetski();
+        dubina();
+    }
 
-	public static void dubina() {
-		System.out.println("Дубина Андрей Юрьевич");
-	}
+    public static void horetski() {
+        System.out.println("Horetski Nikolai Ivanivich");
+    }
+
+    public static void kislushchanka() {
+        System.out.println("Кислущенко Андрей Валерьевич");
+    }
+
+    public static void dubina() {
+        System.out.println("Дубина Андрей Юрьевич");
+    }
 
 }
