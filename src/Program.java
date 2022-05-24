468b54c8db7da0a7bf2b75caf5379d3fa5d4fe80
Sergey Klevtsevich <ipklevtsevich@mail.ru>
2022-05-24 20:55:42 +0300

    Ввел фамилию

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,9 @@ public class Program
 {
 	public static void main(String[] args)
 	{
-
+kliautsevich();
+	}
+	public static void kliautsevich(){
+		System.out.println("Клевцевич Сергей Владимирович");
 	}
 }
