0e7d836423d30a0e5108d26bae48cf7764d4e417
AndreyBeny <dubina2017@yandex.ru>
2022-05-24 20:56:23 +0300

    added method dubina

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,8 @@
-public class Program
-{
-	public static void main(String[] args)
-	{
-
+public class Program {
+    public static void main(String[] args) {
+		dubina();
+    }
+	public static void dubina(){
+		System.out.println("Дубина Андрей Юрьевич");
 	}
 }
