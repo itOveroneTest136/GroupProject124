5a7f3324a4375129c34bb2c3bca3aba2201706b6
lesia khlop <Khlopkova_al@MAIL.RU>
2022-05-24 20:55:45 +0300

    khlopkova coment

--- src/Program.java
+++ src/Program.java
@@ -2,9 +2,12 @@ public class Program
 {
 	public static void main(String[] args)
 	{
+		khlopkova();
 		kislushchanka();
 	}
-
+	public static void khlopkova() {
+		System.out.println("Khlopkova");
+	}
 	public static void kislushchanka(){
 		System.out.println("Кислущенко Андрей Валерьевич");
 	}
