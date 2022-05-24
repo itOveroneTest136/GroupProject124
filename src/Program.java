43445223d2b0f8e08a565510965edb89f05a584a
AndreyBeny <dubina2017@yandex.ru>
2022-05-24 20:56:23 +0300

    added method dubina

--- src/Program.java
+++ src/Program.java
@@ -2,10 +2,6 @@ public class Program
 {
 	public static void main(String[] args)
 	{
-		kislushchanka();
-	}
 
-	public static void kislushchanka(){
-		System.out.println("Кислущенко Андрей Валерьевич");
 	}
 }
